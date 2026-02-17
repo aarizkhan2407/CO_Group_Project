24418430f18520520181352aa62faf4e55fe788f
aarizkhan2407 <aariz.khan2407@gmail.com>
2026-02-17 13:53:46 +0530

    Implemented R-type and I-type instructions

--- assembler.py
+++ assembler.py
@@ -2,6 +2,7 @@ import sys
 
 if ( len(sys.argv) )!=3:
     print("Usage: assembler.py <input.asm> <output.bin>")
+    sys.exit(1)
 
 input_file= sys.argv[1]
 output_file= sys.argv[2]
@@ -28,6 +29,14 @@ R_type= {
     "and":  ("0000000", "111", "0110011"),
 }
 
+I_type = {
+    "addi":  ("000", "0010011"),
+    "lw":    ("010", "0000011"),
+    "sltiu": ("011", "0010011"),
+    "jalr":  ("000", "1100111")
+}
+
+
 def R_encode(operation, rd, rs1, rs2):
     funct7, funct3, opcode= R_type[operation]
     print(funct7+Register_file[rs2]+Register_file[rs1]+funct3+Register_file[rd]+opcode)
@@ -36,25 +45,79 @@ def throw_error(msg, lineno):
     print(f"Error at lineno {lineno}: {msg}")
     sys.exit(1)
 
-with open(input_file, 'r') as fin:
-    PC= 0
-    labels= {}
-    lines= fin.readlines
-    for lineno, line in enumerate(lines):
-        lineno= lineno+1
-        line= line.strip()
-        if (not line):
-            continue
-        if(":" in line):
-            label, inst= line.split(":")
-            labels[label.strip()]= PC
-            line= inst.strip()
-            if (not line):
-                PC+=4
-                continue
-    
-    print(lineno, line)
-    PC+=4
+def BinaryEncoding(value, bits):
+
+    if (value < 0):
+        value= (1<<bits) + value
+    return format(value, f'0{bits}b')
 
 with open(output_file, "w") as fout:
-    fout.write("")
+
+    PC = 0
+
+    with open(input_file, 'r') as fin:
+        lines = fin.readlines()
+
+    for lineno, line in enumerate(lines, start=1):
+
+        line = line.strip()
+
+        if not line:
+            continue
+
+        line = line.replace(",", " ")
+        parts = line.split()
+
+        operation = parts[0]
+
+        # R-TYPE
+        if operation in R_type:
+
+            if len(parts) != 4:
+                throw_error("Invalid operand count", lineno)
+
+            rd, rs1, rs2 = parts[1], parts[2], parts[3]
+
+            binary = (
+                R_type[operation][0]
+                + Register_file[rs2]
+                + Register_file[rs1]
+                + R_type[operation][1]
+                + Register_file[rd]
+                + R_type[operation][2]
+            )
+
+            fout.write(binary + "\n")
+
+        # I-TYPE
+        elif operation in I_type:
+
+            if len(parts) != 4:
+                throw_error("Invalid operand count", lineno)
+
+            rd = parts[1]
+            rs1 = parts[2]
+            imm = int(parts[3])
+
+            imm_bin = BinaryEncoding(imm, 12)
+
+            funct3, opcode = I_type[operation]
+
+            binary = (
+                imm_bin
+                + Register_file[rs1]
+                + funct3
+                + Register_file[rd]
+                + opcode
+            )
+
+            fout.write(binary + "\n")
+
+        else:
+            throw_error("Invalid instruction", lineno)
+
+        PC += 4
+
+
+
+
---
+++ input.asm
@@ -0,0 +1,4 @@
+add s3,s1,zero
+sub s1,s1,s3
+addi s0,s1,120
+sltiu s3,s2,5
